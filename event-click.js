a34363e449fdf3ff61b99136a2f64044690dcdaa
Ara Rosalia Safitri <23106050021@student.uin-suka.ac.id>
2024-12-05 13:56:18 +0700

    Create event-click.js

---
+++ event-click.js
@@ -0,0 +1,6 @@
+const ChangeColorBtn = document.getElementById('ChangeColorBtn');
+const paragraph = document.getElementById('paragraph');
+
+ChangeColorBtn.addEventListener('click', function() {
+    paragraph.style.color = 'red';
+    });
